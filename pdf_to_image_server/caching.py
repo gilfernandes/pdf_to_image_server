7b3cc04f24e42faaddb98fe0a19c3caa32bf41a0
Gil Fernandes <gil.fernandes@onepointltd.com>
2023-08-07 17:17:36 +0100

    Added caching

---
+++ pdf_to_image_server/caching.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from pdf_to_image_server.config import cfg
+from pdf_to_image_server.log_init import logger
+
+def build_cache_name(file_name: str):
+    return cfg.cache_folder/(file_name + ".txt")
+
+
+def read_file(file_name: str) -> Optional[str]:
+    logger.info("Reading file: %s", file_name)
+    file = build_cache_name(file_name)
+    if not file.exists():
+        return None
+    with open(file, 'r') as f:
+        return f.read()
+    
+
+def write_file(content: str, file_name: str):
+    file = build_cache_name(file_name)
+    logger.info("Writing file to: %s", file)
+    with open(file, 'w') as f:
+        f.write(content)
